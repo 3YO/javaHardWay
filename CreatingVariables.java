3f42bbfab7cee092e915e0413554c42cbfe90d09
Bruno <batrillo@gmail.com>
2016-01-12 14:10:08 -0600

    exercise 5

---
+++ CreatingVariables.java
@@ -0,0 +1,31 @@
+public class CreatingVariables {
+	public static void main( String[] args) {
+      int x, y, age, z;
+      double seconds, e, checking, years;
+      String firstName, lastName, title, profession, artistName;
+
+			x = 10;
+			y = 400;
+			age = 39;
+			z = 27;
+
+			seconds = 4.71;
+			e = 2.71828182845904523536;
+			checking = 1.89;
+			years = 3.5;
+
+			firstName = "Graham";
+			lastName = "Mitchell";
+			title = "Mr.";
+			profession = "DJ";
+			artistName = "ANDREII";
+
+			System.out.println( "The variable x contains " + x );
+			System.out.println( "The value " + y + " is stored in the variable y." );
+			System.out.println( "The experiment took " + seconds + " seconds." );
+			System.out.println( "A favorite irrational # is Euler's number: " + e);
+			System.out.println( "Hopefully you have more than $" + checking + "!" );
+			System.out.println( "My name's " + title + " " + firstName + lastName );
+			System.out.println( "I am " + z + " years old, I have been djing for " + years + " years and I go by the name of " + profession + " " + artistName );
+	}
+}
